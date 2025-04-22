8566c1b0bffabf3fd14b33ddbeeab7e87ba37ce2
Darimir <darimrulit@gmail.com>
2025-04-22 15:34:49 +0300

    Почистил код, убрал возвращение промта в чат

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from image_generator import ImageGenerator
 import config
 import logging
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram.ext import Application, CommandHandler
 from config import BOT_TOKEN
 
 logging.basicConfig(
@@ -14,7 +14,6 @@ logger = logging.getLogger(__name__)
 
 async def generate_image(update, context):
     prompt = " ".join(context.args)
-    await update.message.reply_text(prompt)
     pipeline_id = generator.get_pipeline()
     uuid = generator.generate(prompt, pipeline_id)
 
@@ -40,14 +39,13 @@ async def help_command(update, context):
 async def start(update, context):
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я Galereo-бот, могу создавать изображения по запросу. Введите /help чтобы увидеть список команд.",
+        rf"Привет {user.mention_html()}! Я Galereo-бот, могу создавать изображения по запросу. \
+    Введите /help чтобы увидеть список команд.",
     )
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    # text_handler = MessageHandler(filters.TEXT, generate_image)
-    # application.add_handler(text_handler)
     application.add_handler(CommandHandler("gen", generate_image))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("start", start))
